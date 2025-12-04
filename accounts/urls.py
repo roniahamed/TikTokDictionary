83e978fcd1b4006d013c2ba3b5eb61bfbdb257ef
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-05 05:59:53 +0600

    Remove DefaultRouter and update user profile URL in urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,16 +8,13 @@ from rest_framework_simplejwt.views import (
 
 from rest_framework.routers import DefaultRouter
 
-routers = DefaultRouter()
 
-routers.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('registration/', UserRegister.as_view(), name='user-registration'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('', include(routers.urls)),
-    
+    path('profile/', UserViewSet.as_view(), name='user-profile'),    
 
 ]
