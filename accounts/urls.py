567c424c5c9bff0f24cd0d1bafafa2e6f9d95e0a
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-12-05 06:29:13 +0600

    Add change password URL to urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import UserRegister, UserViewSet
+from .views import UserRegister, UserViewSet, changePasswordView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -15,6 +15,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('profile/', UserViewSet.as_view(), name='user-profile'),    
+    path('profile/', UserViewSet.as_view(), name='user-profile'),
+    path('change-password/', changePasswordView.as_view(), name='change-password'),   
 
 ]
